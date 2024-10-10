6bea88f3ca1a669a41813fb43c9a853b808696ca
EmmanuelRM17 <emma041117@gmail.com>
2024-10-10 01:25:29 -0600

    add:pagina

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Componentes Principales
 import Register from './Compartidos/Register';
-
-import LayoutConEncabezado from './Compartidos/LayoutConEncabezado';
+import Home from './Compartidos/Home';
+import Footer from './Compartidos/Footer';
+import Login from './Compartidos/Login'; // Importar el nuevo componente de Login
 
 function App() {
   return (
-  
-    <Router >
-      <Routes>
-        <Route path="/" element={<LayoutConEncabezado> <Register /> </LayoutConEncabezado> } />
-      
-      
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Página principal */}
+          <Route path="/register" element={<Register />} /> {/* Página de registro */}
+          <Route path="/login" element={<Login />} /> {/* Página de inicio de sesión */}
+        </Routes>
+      </Router>
+      <Footer /> {/* Pie de página */}
+    </div>
   );
-};
+}
 
 export default App;
