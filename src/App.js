05c9e440b400323acc73787672fd2c55a347e571
GerardoDomingo <20221110@uthh.edu.mx>
2024-10-10 00:53:45 -0600

    add:front

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Componentes Principales
-import   Register from './Compartidos/Register';
+import Register from './Compartidos/Register';
+
+import LayoutConEncabezado from './Compartidos/LayoutConEncabezado';
 
 function App() {
   return (
   
     <Router >
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<LayoutConEncabezado> <Register /> </LayoutConEncabezado> } />
       
       
       </Routes>
---
+++ src/Compartidos/LayoutConEncabezado.js
@@ -0,0 +1,25 @@
+// src/componentes/LayoutConEncabezado.js
+import React from 'react';
+import BarraNav from './BarraNav';
+import PieDePagina from './Footer'; // Asegúrate de importar el Footer correctamente
+import { Layout } from 'antd';
+
+const { Content } = Layout;
+
+const LayoutConEncabezado = ({ children }) => {
+  return (
+    <Layout className="layout">
+      <header>
+        <BarraNav />
+      </header>
+      <Content style={{ padding: '20px', backgroundColor: '#f0f2f5' }}>
+        {children}
+      </Content>
+      <footer>
+        <PieDePagina />
+      </footer>
+    </Layout>
+  );
+}
+
+export default LayoutConEncabezado;
