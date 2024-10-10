c8b2c4621f1e015bb49a50733fd3c6bb594be4ff
GerardoDomingo <20221110@uthh.edu.mx>
2024-10-09 21:20:26 -0600

    add:front

--- src/Compartidos/Register.js
+++ src/Compartidos/Register.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import axios from 'axios';
 import 'bulma/css/bulma.css'; // Importar Bulma
+import { FaCheckCircle } from 'react-icons/fa'; // Importar un ícono para la verificación
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -14,6 +15,11 @@ const Register = () => {
   });
 
   const [passwordError, setPasswordError] = useState('');
+  const [emailError, setEmailError] = useState(''); // Estado para el error de correo
+  const [isPasswordSafe, setIsPasswordSafe] = useState(false); // Estado para controlar si la contraseña es segura
+  const [isModalVisible, setIsModalVisible] = useState(false); // Estado para mostrar el modal
+  const [modalMessage, setModalMessage] = useState(''); // Mensaje del modal
+  const [isSuccess, setIsSuccess] = useState(false); // Estado para saber si el mensaje es de éxito
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -22,9 +28,15 @@ const Register = () => {
       [name]: value,
     }));
 
-    // Resetear error de contraseña cuando el usuario cambia la contraseña
+    // Resetear error de contraseña y estado de seguridad cuando el usuario cambia la contraseña
     if (name === 'password') {
       setPasswordError('');
+      setIsPasswordSafe(false); // Resetear la verificación de seguridad
+    }
+
+    // Resetear el error de correo cuando el usuario modifica el campo
+    if (name === 'email') {
+      setEmailError('');
     }
   };
 
@@ -37,46 +49,62 @@ const Register = () => {
       // Comprobar si la contraseña ha sido expuesta
       if (hashes.includes(hashedPassword.slice(5))) {
         setPasswordError('Contraseña insegura: ha sido filtrada en brechas de datos.');
+        setIsPasswordSafe(false); // Contraseña no es segura
+      } else {
+        setIsPasswordSafe(true); // Contraseña es segura
       }
     } catch (error) {
       console.error('Error al verificar la contraseña:', error);
     }
   };
 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     // Verificar si la contraseña es segura
     await checkPasswordSafety(formData.password);
+    // Validar la contraseña antes de proceder
+    if (formData.password !== formData.confirmPassword) {
+      showModal('Las contraseñas no coinciden', false); // Mostrar modal de error
+      return; // Detener el registro si las contraseñas no coinciden
+    }
 
-    // Solo continuar si no hay errores de contraseña
-    if (!passwordError && formData.password === formData.confirmPassword) {
-      try {
-        const response = await axios.post('http://localhost:3001/api/users/register', formData);
-        alert(response.data); // Mostrar mensaje de éxito
-        // Limpiar el formulario
-        setFormData({
-          nombre: '',
-          aPaterno: '',
-          aMaterno: '',
-          telefono: '',
-          email: '',
-          password: '',
-          confirmPassword: '', // Limpiar el campo de confirmar contraseña
-        });
-      } catch (error) {
-        alert('Error al registrar el usuario');
-        console.error('Error en el registro:', error);
-      }
-    } else if (formData.password !== formData.confirmPassword) {
-      alert('Las contraseñas no coinciden');
+    // Intentar registrar al usuario
+    try {
+      const response = await axios.post('http://localhost:3001/api/users/register', formData);
+      showModal(response.data.message, true); // Mostrar modal de éxito
+      showModal('Usuario registrado con éxito', true); // Cambia esta línea
+      // Limpiar el formulario
+      setFormData({
+        nombre: '',
+        aPaterno: '',
+        aMaterno: '',
+        telefono: '',
+        email: '',
+        password: '',
+        confirmPassword: '', // Limpiar el campo de confirmar contraseña
+      });
+      setIsPasswordSafe(false); // Resetear el estado de seguridad de la contraseña
+    } catch (error) {
+      showModal('Error al registrar el usuario el, correo ya existe ', false); // Mostrar modal de error
+      console.error('Error en el registro:', error);
     }
   };
 
-  return (
-<div style={{ backgroundColor: '#FFFFFF', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <div className="card" style={{ backgroundColor: '#FFFFFF',maxWidth: '800px', width: '100%', borderRadius: '10px', border: '1px solid #dcdcdc' }}>
+  const showModal = (message, success) => {
+    setModalMessage(message);
+    setIsSuccess(success);
+    setIsModalVisible(true);
+  };
 
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div style={{ backgroundColor: '#FFFFFF', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div className="card" style={{ backgroundColor: '#FFFFFF', maxWidth: '800px', width: '100%', borderRadius: '10px', border: '1px solid #dcdcdc' }}>
         <header className="card-header" style={{ backgroundColor: '#00bcd4', color: '#ffffff', borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }}>
           <p className="card-header-title has-text-centered">Registro de Paciente</p>
         </header>
@@ -111,6 +139,7 @@ const Register = () => {
               <div className="control">
                 <input className="input" type="email" name="email" value={formData.email} onChange={handleChange} required style={{ backgroundColor: '#ffffff', color: '#000000' }} />
               </div>
+              {emailError && <p className="help is-danger">{emailError}</p>} {/* Mostrar mensaje de error de correo */}
             </div>
             <div className="field">
               <label className="label" style={{ color: '#000000' }}>Contraseña:</label>
@@ -118,6 +147,7 @@ const Register = () => {
                 <input className="input" type="password" name="password" value={formData.password} onChange={handleChange} required style={{ backgroundColor: '#ffffff', color: '#000000' }} />
               </div>
               {passwordError && <p className="help is-danger">{passwordError}</p>}
+              {isPasswordSafe && <p className="help is-success"><FaCheckCircle style={{ color: 'green' }} /> Contraseña segura</p>}
             </div>
             <div className="field">
               <label className="label" style={{ color: '#000000' }}>Confirmar Contraseña:</label>
@@ -125,10 +155,27 @@ const Register = () => {
                 <input className="input" type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} required style={{ backgroundColor: '#ffffff', color: '#000000' }} />
               </div>
             </div>
-            <button type="submit" className="button is-primary is-fullwidth">Registrarse</button>
+            <div className="control">
+              <button className="button is-primary" type="submit" style={{ width: '100%' }}>Registrarse</button>
+            </div>
           </form>
         </div>
       </div>
+{/* Modal para mostrar mensajes */}
+{isModalVisible && (
+  <div className="modal is-active">
+    <div className="modal-background" onClick={closeModal}></div>
+    <div className="modal-content" style={{ backgroundColor: isSuccess ? '#d4edda' : '#f8d7da', borderRadius: '8px', padding: '20px', textAlign: 'center' }}>
+      <p style={{ color: isSuccess ? '#155724' : '#721c24', fontWeight: 'bold', marginBottom: '20px' }}>
+        {modalMessage}
+      </p>
+      <button className="button is-info" onClick={closeModal} style={{ margin: '0 auto' }}>Cerrar</button>
+    </div>
+    <button className="modal-close is-large" aria-label="close" onClick={closeModal}></button>
+  </div>
+)}
+
+
     </div>
   );
 };
