8c98e0ee697623d5a1659b1807b10918c8c5a9a6
EmmanuelRM17 <emma041117@gmail.com>
2024-10-21 00:51:56 -0600

    nose

--- src/Inicio/Login.jsx
+++ src/Inicio/Login.jsx
@@ -13,27 +13,8 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [openNotification, setOpenNotification] = useState(false);
   const [notificationMessage, setNotificationMessage] = useState('');
-  const [csrfToken, setCsrfToken] = useState(''); // Almacenar el token CSRF
   const recaptchaRef = useRef(null);
   const navigate = useNavigate();
-
-  // Obtener el token CSRF al montar el componenteee
-  useEffect(() => {
-    const fetchCsrfToken = async () => {
-      try {
-        const response = await fetch('https://backendodontologia.onrender.com/api/csrf-token', {
-          method: 'GET',
-          credentials: 'include',  // Importante para incluir las cookies de sesión
-        });
-        const data = await response.json();
-        setCsrfToken(data.csrfToken);  // Guarda el token en el estado
-      } catch (error) {
-        console.error('Error al obtener el token CSRF:', error);
-      }
-    };
-
-    fetchCsrfToken();
-}, []);
 
   // Eliminar mensaje de error después de 3 segundos
   useEffect(() => {
@@ -46,23 +27,28 @@ const Login = () => {
     return () => clearTimeout(errorTimeout);
   }, [errorMessage]);
 
+  // Manejar el cambio de los campos del formulariooo
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
+  // Capturar el valor del captcha cuando se completa
   const handleCaptchaChange = (value) => {
     setCaptchaValue(value);
   };
 
+  // Alternar la visibilidad de la contraseña
   const handleTogglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  // Cerrar la notificación
   const handleCloseNotification = () => {
     setOpenNotification(false);
   };
 
+  // Manejar el envío del formulario
   const handleSubmit = async (e) => {
     e.preventDefault();
   
@@ -76,31 +62,33 @@ const Login = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'CSRF-Token': csrfToken,  // Enviar el token CSRF en el encabezado
         },
         body: JSON.stringify({ ...formData, captchaValue }),
-        credentials: 'include', // Incluir cookies en la solicitud
       });
   
       const data = await response.json();
   
       if (response.ok) {
+        // Verificar el tipo de usuario y redirigir
         if (data.user.tipo === 'administrador') {
-          navigate('/Administrador/principal');
+          navigate('/Administrador/principal');  // Redirigir a la página del administrador
         } else if (data.user.tipo === 'paciente') {
-          navigate('/Paciente/principal');
+          navigate('/Paciente/principal');  // Redirigir a la página del paciente
         }
       } else {
+        // Mostrar notificación y resetear el captcha
         setNotificationMessage(`Intentos fallidos: ${data.failedAttempts || 0}`);
         setOpenNotification(true);
-        recaptchaRef.current.reset();
-        setCaptchaValue(null);
+        recaptchaRef.current.reset(); // Resetear el captcha
+        setCaptchaValue(null); // Deshabilitar el botón de nuevo
         setErrorMessage(data.message || 'Error al iniciar sesión');
       }
     } catch (error) {
       setErrorMessage('Error de conexión. Inténtalo de nuevo más tarde.');
     }
   };
+  
+
   return (
     <Box
       sx={{
@@ -116,7 +104,7 @@ const Login = () => {
       <IconButton
         sx={{ position: 'absolute', top: 16, left: 16, color: '#00bcd4' }}
         component={Link}
-        to="/"
+        to="/" 
       >
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
           <ArrowBack />
@@ -125,7 +113,7 @@ const Login = () => {
           </Typography>
         </Box>
       </IconButton>
-
+      
       <Card sx={{ maxWidth: 400, width: '100%', borderRadius: '15px', boxShadow: 3, position: 'relative' }}>
         <CardContent sx={{ textAlign: 'center', p: 4 }}>
           <IconButton sx={{ fontSize: 40, color: '#00bcd4' }}>
