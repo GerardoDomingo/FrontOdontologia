dfc2aae74592f9a5cffb54dccb21ad5cdb23a150
EmmanuelRM17 <emma041117@gmail.com>
2024-12-19 14:16:59 -0600

    claro

--- src/Inicio/Preguntas.jsx
+++ src/Inicio/Preguntas.jsx
@@ -32,7 +32,7 @@ const FAQ = () => {
 
   // System Theme Detection
   useEffect(() => {
-    setIsDarkMode(false);
+    setIsDarkTheme(false);
     const matchDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
     setIsDarkTheme(matchDarkTheme.matches);
 
